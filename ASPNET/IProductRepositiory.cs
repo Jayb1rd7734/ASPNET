0c378c0ad1136fc45ed76129ec6e105068dd4bc6
Jay Snodderly <j.smedederly@gmail.com>
2020-03-05 20:14:25 -0600

    Class 3/5/20.

--- ASPNET/IProductRepositiory.cs
+++ ASPNET/IProductRepositiory.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNET.Models;
 
 namespace ASPNET
 {
-    interface IProductRepositiory
+    public interface IProductRepositiory
     {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
     }
 }
--- ASPNET/ProductRepository.cs
+++ ASPNET/ProductRepository.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNET.Models;
+using Dapper;
 
 namespace ASPNET
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepositiory
     {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+
+        public Product GetProduct( int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @id",
+                new { id = id });
+        }
     }
 }
